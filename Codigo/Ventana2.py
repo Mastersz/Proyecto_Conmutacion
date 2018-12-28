331460e9fec1b188e1d988d12d342314213e68df
dtgarzon <dtgarzon@espol.edu.ec>
2018-12-28 12:45:29 -0500

    ventana de conxión ssh

--- Codigo/Ventana2.py
+++ Codigo/Ventana2.py
@@ -64,23 +64,90 @@ class FirstFrame(tk.Frame):
             c = db.cursor()
 
         # Find user If there is any take proper action
-        find_user = ('SELECT * FROM user WHERE username = ? and password = ?')
-        c.execute(find_user, [(self.usr.get()), (self.pwd.get())])
+        find_user = ('SELECT * FROM user WHERE username = ? ')
+        c.execute(find_user, [(self.usr.get())])
         result = c.fetchall()
 
-        if result:
-            tm.showinfo("Login info",self.usr.get() + '\n Bienvenid@')
-            self.master.change(SecondFrame)
-        elif self.usr.get() == '' and self.pwd.get() == '':
+        if self.usr.get() == '' and self.pwd.get() == '':
             tm.showerror("Login error", "Ingrese información")
         elif self.usr.get() == '':
             tm.showerror("Login error", "Usuario faltante")
         elif self.pwd.get() == '':
             tm.showerror("Login error", "Contraseña faltante")
-
+        elif result:
+            find_pwd = ('SELECT * FROM user WHERE password = ? ')
+            c.execute(find_pwd, [(self.pwd.get())])
+            pwd_correcto = c.fetchall()
+            if pwd_correcto:
+                tm.showinfo("Login info", self.usr.get() + '\n Bienvenid@')
+                self.master.change(ConfigFrame)
+            else:
+                ms.showerror('Oops!', 'Contraseña incorrecta.')
         else:
-            ms.showerror('Oops!', 'Nombre de usuario o contraseña incorrecto.')
+            ms.showerror('Oops!', 'Nombre de usuario incorrecto.')
+        # Frame Packing Methords
 
+class ConfigFrame(tk.Frame):
+    def __init__(self, master=None, **kwargs):
+        "Ventana principal que contiene el inicio de sesiòn del usuario"
+        tk.Frame.__init__(self, master, **kwargs)
+
+        master.title("Conectar por SSH")
+        master.geometry("300x200")
+        self.status = tk.Label(self, fg='red')
+        self.status.pack()
+        ip = tk.Label(self, text='IP', font=('', 10))
+        ip.pack()
+        self.ip = tk.Entry(self)
+        self.ip.pack()
+        self.ip.focus()
+        usr = tk.Label(self, text='Usuario', font=('', 10))
+        usr.pack()
+        self.usr = tk.Entry(self)
+        self.usr.pack()
+        self.usr.focus()
+        lbl = tk.Label(self, text='Contraseña', font=('', 10))
+        lbl.pack()
+        self.pwd = tk.Entry(self, show="*")
+        self.pwd.pack()
+        self.pwd.focus()
+        self.pwd.bind('<Return>', self.check2)
+        btn = tk.Button(self, text="Conectar", font=('', 10), command=self.check2)
+
+        btn.pack()
+
+    def check2(self, event=None):
+        '''Chequea los eventos del botòn Login
+            Si los datos son correctos retorna Bienvenido (Nombre del usuario) y la siguiente ventana
+            Si la contraseña es incorrecta retorna el mensaje Contraseña incorrecta
+            Si falta informaciòn retorna el respectivo mensaje con la informaciòn que falte
+        '''
+        with sqlite3.connect('DataBase.db') as db:
+            c = db.cursor()
+
+        # Find user If there is any take proper action
+        find_user = ('SELECT * FROM user WHERE username = ? ')
+        c.execute(find_user, [(self.usr.get())])
+        result = c.fetchall()
+
+        if self.usr.get() == '' and self.pwd.get() == ''and self.ip.get() == '':
+            tm.showerror("Login error", "Ingrese información")
+        elif self.ip.get() == '':
+            tm.showerror("Login error", "IP faltante")
+        elif self.usr.get() == '':
+            tm.showerror("Login error", "Usuario faltante")
+        elif self.pwd.get() == '':
+            tm.showerror("Login error", "Contraseña faltante")
+        elif result:
+            find_pwd = ('SELECT * FROM user WHERE password = ? ')
+            c.execute(find_pwd, [(self.pwd.get())])
+            pwd_correcto = c.fetchall()
+            if pwd_correcto:
+                self.master.change(SecondFrame)
+            else:
+                ms.showerror('Oops!', 'Contraseña incorrecta.')
+        else:
+            ms.showerror('Oops!', 'Nombre de usuario incorrecto.')
         # Frame Packing Methords
 
 
@@ -106,16 +173,16 @@ class SecondFrame(tk.Frame):
 
         rbtn_val = tk.IntVar()
         rbtn_val.set(1)
-        lbl = tk.Label(self, text='Escoja la configuración a realizar')
-        r_btn1 = tk.Radiobutton(self, text='P', padx=20, variable=rbtn_val, value=1)
-        r_btn2 = tk.Radiobutton(self, text='CE', padx=20, variable=rbtn_val, value=2)
-        r_btn3 = tk.Radiobutton(self, text='PE', padx=20, variable=rbtn_val, value=3)
+        lbl = tk.Label(self, text='Escoja la configuración a realizar', font=('', 12))
+        r_btn1 = tk.Radiobutton(self, text='P', padx=20, variable=rbtn_val, value=1, font=('', 12))
+        r_btn2 = tk.Radiobutton(self, text='CE', padx=20, variable=rbtn_val, value=2, font=('', 12))
+        r_btn3 = tk.Radiobutton(self, text='PE', padx=20, variable=rbtn_val, value=3, font=('', 12))
         lbl.pack()
         r_btn1.pack()
         r_btn2.pack()
         r_btn3.pack()
-        btn_continuar = tk.Button(self, text="Continuar", command=configuration)
-        btn_log_out = tk.Button(self, text="Cerrar Sesiòn", command=lambda: self.master.change(FirstFrame))
+        btn_continuar = tk.Button(self, text="Continuar", command=configuration, font=('', 12))
+        btn_log_out = tk.Button(self, text="Cerrar Sesiòn", command=lambda: self.master.change(FirstFrame), font=('', 12))
         btn_continuar.pack()
         btn_log_out.pack()
 
